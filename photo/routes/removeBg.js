30bcab9fef25ab70c52cb778ce042f584e0d1a9a
Arshath <elyasarshath6963gmail.com>
2026-03-24 15:17:12 +0530

    change removebg.js

--- photo/routes/removeBg.js
+++ photo/routes/removeBg.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const { exec } = require('child_process');
 const cloudinary = require('cloudinary').v2;
+
 const router = express.Router();
 
 
@@ -12,18 +13,31 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) =>
+    cb(null, Date.now() + path.extname(file.originalname))
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 100 * 1024 * 1024 } 
+});
+
+
+router.get('/', (req, res) => {
+  res.send("Remove BG API working");
+});
+
 
 router.post('/', upload.single('image'), (req, res) => {
+  console.log("Request received");
+
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
   const inputPath = req.file.path;
   const outputPath = `outputs/output-${Date.now()}.png`;
 
@@ -31,26 +45,27 @@ router.post('/', upload.single('image'), (req, res) => {
     process.platform === "win32"
       ? `python python/remove_bg.py ${inputPath} ${outputPath}`
       : `python3 python/remove_bg.py ${inputPath} ${outputPath}`;
+
   exec(command, async (error, stdout, stderr) => {
     if (error) {
-      console.error(error);
+      console.error("Python error:", error);
       return res.status(500).json({ error: 'Background removal failed' });
     }
 
-    
     try {
-      const result = await cloudinary.uploader.upload(outputPath, {
-        folder: 'rembg-outputs'
+    
+      const result = await cloudinary.uploader.upload_large(outputPath, {
+        resource_type: "image",
+        folder: "rembg-outputs"
       });
 
       res.json({
         message: 'Background removed successfully',
-        output: result.secure_url  // Cloudinary URL
-
+        output: result.secure_url
       });
 
     } catch (uploadError) {
-      console.error(uploadError);
+      console.error("Cloudinary error:", uploadError);
       res.status(500).json({ error: 'Cloudinary upload failed' });
     }
   });
